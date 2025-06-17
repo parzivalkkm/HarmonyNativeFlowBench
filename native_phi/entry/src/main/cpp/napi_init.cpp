a10af4deed523d37e23529457f0d96b4b277e089
Parzival <2833456550@qq.com>
2025-06-17 17:15:38 +0800

    更新phi测试用例

--- native_phi/entry/src/main/cpp/napi_init.cpp
+++ native_phi/entry/src/main/cpp/napi_init.cpp
@@ -1,5 +1,6 @@
 #include "napi/native_api.h"
 #include "hilog/log.h"
+#include <cstring>
 
 static napi_value BranchPhiTest(napi_env env, napi_callback_info info)
 {
@@ -28,37 +29,83 @@ static napi_value BranchPhiTest(napi_env env, napi_callback_info info)
 
 static napi_value ContractPhiTest(napi_env env, napi_callback_info info)
 {
-    napi_value a1;
-    napi_create_int64(env, 1, &a1);
-    napi_value a2;
-    napi_create_int64(env, 2, &a2);
-    int x = 65534;
-    if (x*(x+1)%2 == 0){
-        return a1;
-    }else{
-        return a2;
-    }
+    size_t argc = 2;
+    napi_value args[2];
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    
+    // 获取字符串长度
+    size_t str1_length = 0;
+    size_t str2_length = 0;
+    napi_get_value_string_utf8(env, args[0], nullptr, 0, &str1_length);
+    napi_get_value_string_utf8(env, args[1], nullptr, 0, &str2_length);
+    
+    // 分配内存空间
+    char* str1 = new char[str1_length + 1];
+    char* str2 = new char[str2_length + 1];
+    char* result = new char[str1_length + str2_length + 1];
+    
+    // 获取字符串内容
+    napi_get_value_string_utf8(env, args[0], str1, str1_length + 1, nullptr);
+    napi_get_value_string_utf8(env, args[1], str2, str2_length + 1, nullptr);
+    
+    // 使用 strcat 连接字符串
+    strcpy(result, str1);
+    strcat(result, str2);
+    
+    // 创建返回值
+    napi_value output;
+    napi_create_string_utf8(env, result, NAPI_AUTO_LENGTH, &output);
+    
+    // 释放内存
+    delete[] str1;
+    delete[] str2;
+    delete[] result;
+    
+    return output;
 }
 
 static napi_value ContractPhiLeakTest(napi_env env, napi_callback_info info)
 {
-    size_t argc = 1;
-    napi_value args[1];
+    size_t argc = 2;
+    napi_value args[2];
     napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
     
-    char* str = new char[256];
+    // 获取字符串长度
+    size_t str1_length = 0;
+    size_t str2_length = 0;
+    napi_get_value_string_utf8(env, args[0], nullptr, 0, &str1_length);
+    napi_get_value_string_utf8(env, args[1], nullptr, 0, &str2_length);
     
-    napi_get_value_string_utf8(env, args[0], str, NAPI_AUTO_LENGTH, nullptr);
+    // 分配内存
+    char* str1 = new char[str1_length + 1];
+    char* str2 = new char[str2_length + 1];
+    char* result = new char[str1_length + str2_length + 1];
     
-    char* contracted = new char[512];
+    // 获取字符串内容
+    napi_get_value_string_utf8(env, args[0], str1, str1_length + 1, nullptr);
+    napi_get_value_string_utf8(env, args[1], str2, str2_length + 1, nullptr);
     
-    OH_LOG_INFO(LOG_APP, "Leak LastLocation %{public}s", str);
+    // 使用 strcat 连接字符串
+    strcpy(result, str1);
+    strcat(result, str2);
+    
+    // 记录日志
+    OH_LOG_INFO(LOG_APP, "StrCat result: %{public}s + %{public}s = %{public}s", str1, str2, result);
+    
+    // 创建返回值
+    napi_value output;
+    napi_create_string_utf8(env, result, NAPI_AUTO_LENGTH, &output);
+    
+    // 释放内存
+    delete[] str1;
+    delete[] str2;
+    delete[] result;
+      return output;
 }
 
 EXTERN_C_START
 static napi_value Init(napi_env env, napi_value exports)
-{
-    napi_property_descriptor desc[] = {
+{    napi_property_descriptor desc[] = {
         {"branchPhiTest", nullptr, BranchPhiTest, nullptr, nullptr, nullptr, napi_default, nullptr},
         {"contractPhiTest", nullptr, ContractPhiTest, nullptr, nullptr, nullptr, napi_default, nullptr},
         {"contractPhiLeakTest", nullptr, ContractPhiLeakTest, nullptr, nullptr, nullptr, napi_default, nullptr},
