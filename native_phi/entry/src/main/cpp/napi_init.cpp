46423a728ff8a73ea24c1a36fda57893049de622
Parzival <2833456550@qq.com>
2025-06-18 16:21:48 +0800

    phi测试用例更改了仅使用strcat的情况

--- native_phi/entry/src/main/cpp/napi_init.cpp
+++ native_phi/entry/src/main/cpp/napi_init.cpp
@@ -77,30 +77,27 @@ static napi_value ContractPhiLeakTest(napi_env env, napi_callback_info info)
     napi_get_value_string_utf8(env, args[1], nullptr, 0, &str2_length);
     
     // 分配内存
-    char* str1 = new char[str1_length + 1];
+    char* str1 = new char[str1_length + str2_length + 1];
     char* str2 = new char[str2_length + 1];
-    char* result = new char[str1_length + str2_length + 1];
     
     // 获取字符串内容
     napi_get_value_string_utf8(env, args[0], str1, str1_length + 1, nullptr);
     napi_get_value_string_utf8(env, args[1], str2, str2_length + 1, nullptr);
     
     // 使用 strcat 连接字符串
-    strcpy(result, str1);
-    strcat(result, str2);
+    strcat(str1, str2);
     
     // 记录日志
-    OH_LOG_INFO(LOG_APP, "StrCat result: %{public}s + %{public}s = %{public}s", str1, str2, result);
+    OH_LOG_INFO(LOG_APP, "StrCat result: str1 + str2 = %{public}s", str1);
     
     // 创建返回值
     napi_value output;
-    napi_create_string_utf8(env, result, NAPI_AUTO_LENGTH, &output);
+    napi_create_string_utf8(env, str1, NAPI_AUTO_LENGTH, &output);
     
     // 释放内存
     delete[] str1;
     delete[] str2;
-    delete[] result;
-      return output;
+    return output;
 }
 
 EXTERN_C_START
